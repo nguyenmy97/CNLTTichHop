cd4ce0e48b801b6bf043a22f6e74b7bb28c0b823
Nguyễn Công Thắng <CongThang1597@users.noreply.github.com>
2018-12-04 16:16:26 +0700

    Create Client

---
+++ ServerAPI/Models/connect.js
@@ -0,0 +1,11 @@
+const { Pool, Client } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'ThiTN',
+    password: 'nct',
+    port: 5433,
+});
+
+module.exports = pool
